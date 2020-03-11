b730655ec6fddfea531f2e826ebe57d014f3efb3
Ihor Kovalyk <Ihor_Kovalyk@epam.com>
2020-03-11 20:58:05 +0200

    Created enums for digits

--- src/main/java/com/kata/PipelineDigit.java
+++ src/main/java/com/kata/PipelineDigit.java
@@ -1,5 +1,38 @@
 package com.kata;
 
 public enum PipelineDigit {
+    ONE("   " +
+            "  |" +
+            "  |"),
+    TWO(" _ " +
+            " _|" +
+            "|_ "),
+    THREE(" _ " +
+            " _|" +
+            " _|"),
+    FOUR("   " +
+            "|_|" +
+            "  |"),
+    FIVE(" _ " +
+            "|_ " +
+            " _|"),
+    SIX(" _ " +
+            "|_ " +
+            "|_|"),
+    SEVEN(" _ " +
+            "  |" +
+            "  |"),
+    EIGHT(" _ " +
+            "|_|" +
+            "|_|"),
+    NINE(" _ " +
+            "|_|" +
+            " _|"),
+    ZERO(" _ " +
+            "| |" +
+            "|_|");
+
+    PipelineDigit(String s) {
+    }
 
     }
