1e7e2e9d03b39d9281eaab603f5f71755c76cbf0
Ihor Kovalyk <777kovalik@gmail.com>
2020-03-20 23:55:58 +0200

    Started String version of kata

---
+++ src/main/java/com/kata/PipeLineDigit.java
@@ -0,0 +1,68 @@
+package com.kata;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PipeLineDigit {
+    public static final String ZERO =
+            " _ \n" +
+                    "| |\n" +
+                    "|_|\n";
+
+    public static final String ONE =
+            "   \n" +
+                    "  |\n" +
+                    "  |\n";
+    public static final String TWO =
+            " _ \n" +
+                    " _|\n" +
+                    "|_ \n";
+    public static final String THREE =
+            " _\n " +
+                    " _|\n" +
+                    " _|\n";
+    public static final String FOUR =
+            "   \n" +
+                    "|_|\n" +
+                    "  |\n";
+    public static final String FIVE =
+            " _\n " +
+                    "|_\n " +
+                    " _|\n";
+    public static final String SIX =
+            " _ \n" +
+                    "|_ \n" +
+                    "|_|\n";
+    public static final String SEVEN =
+            " _ \n" +
+                    "  |\n" +
+                    "  |\n";
+    public static final String EIGHT =
+            " _\n " +
+                    "|_|\n" +
+                    "|_|\n";
+    public static final String NINE =
+            " _ \n" +
+                    "|_|\n" +
+                    " _|\n";
+
+    public Map<String, Integer> digits;
+
+    public void getDigit(String s) {
+        int i = 0;
+        digits = new HashMap<>();
+        digits.put(ZERO, 0);
+        digits.put(ONE, 1);
+        digits.put(TWO, 2);
+        digits.put(THREE, 3);
+        digits.put(FOUR, 4);
+        digits.put(FIVE, 5);
+        digits.put(SIX, 6);
+        digits.put(SEVEN, 7);
+        digits.put(EIGHT, 8);
+        digits.put(NINE, 9);
+        digits.forEach((key,value) -> {
+            System.out.println(value);});
+
+    }
+}
